899f782b92b603ecab98a17ed069e69df2af7745
David Veytsman <dveyts@yahoo.com>
2018-02-01 16:34:22 -0500

    added functionality

--- question_likes.rb
+++ question_likes.rb
@@ -10,6 +10,18 @@ class QuestionLike
     data.map { |question_like| QuestionLike.new(question_like) }
   end
 
+  def self.find_by_id(id)
+    question_like = QuestionsDatabase.instance.execute(<<-SQL, id: id)
+      SELECT
+        *
+      FROM
+        question_likes
+      WHERE
+        id = :id
+    SQL
+    QuestionLike.new(question_like.first)
+  end
+  
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
