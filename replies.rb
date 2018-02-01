97ee4f45728a7573608483516ba996de16d9006e
David Veytsman <dveyts@yahoo.com>
2018-02-01 15:15:22 -0500

    added the reply class

---
+++ replies.rb
@@ -0,0 +1,20 @@
+
+require 'SQLite3'
+require_relative 'questions_database'
+
+class Reply
+  attr_accessor :user_id, :question_id, :parent_reply_id, :body
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
+    data.map { |reply| Reply.new(reply) }
+  end
+
+  def initialize(options)
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+    @parent_reply_id = options['parent_reply_id']
+    @body = options['body']
+  end
+
+end
