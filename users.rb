a38d34b0c72a4915539132d4b23f17c015b51ef2
David Veytsman <dveyts@yahoo.com>
2018-02-01 16:37:22 -0500

    added functionality

--- users.rb
+++ users.rb
@@ -1,9 +1,11 @@
 
 require 'SQLite3'
 require_relative 'questions_database'
+require_relative 'questions'
 
 class User
   attr_accessor :fname, :lname
+  attr_reader :id
 
   def initialize(options)
     @id = options['id']
@@ -11,8 +13,37 @@ class User
     @lname = options['lname']
   end
 
+  def self.find_by_id(id)
+    user = QuestionsDatabase.instance.execute(<<-SQL, id: id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = :id
+    SQL
+    User.new(user.first)
+  end
+
+  def self.find_by_name(fname, lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname: fname, lname: lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = :fname AND lname = :lname
+    SQL
+
+    User.new(user.first)
+  end
+
   def self.all
     data = QuestionsDatabase.instance.execute("SELECT * FROM users")
     data.map { |user| User.new(user)  }
+  end
+
+  def authored_questions
+    Question.find_by_author_id(id)
   end
 end
