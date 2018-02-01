cc6339aff4e523c58fd718790246769a9fa73d16
David Veytsman <dveyts@yahoo.com>
2018-02-01 15:24:36 -0500

    using the id instance

--- replies.rb
+++ replies.rb
@@ -11,6 +11,7 @@ class Reply
   end
 
   def initialize(options)
+    @id = options['id']
     @question_id = options['question_id']
     @user_id = options['user_id']
     @parent_reply_id = options['parent_reply_id']
