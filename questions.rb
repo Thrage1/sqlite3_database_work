372bbcb2c2dd80f613009ccbfbb20b2fadb069c0
David Veytsman <dveyts@yahoo.com>
2018-02-01 16:34:40 -0500

    added functionality

--- questions.rb
+++ questions.rb
@@ -11,6 +11,29 @@ class Question
     data.map { |question| Question.new(question) }
   end
 
+  def self.find_by_author_id(author_id)
+    questions = QuestionsDatabase.instance.execute(<<-SQL, author_id: author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        author_id = :author_id
+    SQL
+    questions.map { |question| Question.new(question) }
+  end
+
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, id: id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = :id
+    SQL
+    Question.new(question.first)
+  end
 
   def initialize(options)
     @id = options['id']
@@ -18,4 +41,6 @@ class Question
     @body = options['body']
     @author_id = options['author_id']
   end
+
+
 end
