85d832ba6d6eb041f1704b3d673203bad0741951
David Veytsman <dveyts@yahoo.com>
2018-02-01 16:34:58 -0500

    added functionality

--- replies.rb
+++ replies.rb
@@ -10,6 +10,30 @@ class Reply
     data.map { |reply| Reply.new(reply) }
   end
 
+  def self.find_by_id(id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, id: id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = :id
+    SQL
+    Reply.new(reply.first)
+  end
+
+  def self.find_by_user_id(user_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, user_id :user_id)
+    SELECT
+      *
+    FROM
+      replies
+    WHERE
+      user_id = :user_id
+    SQL
+    replies.map { |reply| Reply.new(reply) }
+  end
+
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
