b873f60833e9ef75239343f5e1e135f37f03153f
David Veytsman <dveyts@yahoo.com>
2018-02-01 16:34:06 -0500

    added functionality

--- question_follows.rb
+++ question_follows.rb
@@ -1,6 +1,7 @@
 
 require 'SQLite3'
 require_relative 'questions_database'
+require_relative 'users'
 
 class QuestionFollow
   attr_accessor :user_id, :question_id
@@ -8,6 +9,22 @@ class QuestionFollow
   def self.all
     data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
     data.map { |question_follow| QuestionFollow.new(question_follow) }
+  end
+
+  def self.find_by_id(id)
+    question_follows = QuestionsDatabase.instance.execute(<<-SQL, id: id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        id = :id
+    SQL
+    QuestionFollow.new(question_follows.first)
+  end
+
+  def self.find_by_author_id(author_id)
+    User.find_by_id(author_id)
   end
 
   def initialize(options)
