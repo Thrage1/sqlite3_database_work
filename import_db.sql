3022b1164a574c95e4ca357bd66b9ddb4bceaefb
David Veytsman <dveyts@yahoo.com>
2018-02-01 13:52:58 -0500

    edited code

--- import_db.sql
+++ import_db.sql
@@ -15,7 +15,7 @@ CREATE TABLE questions(
   title TEXT NOT NULL,
   body TEXT NOT NULL,
   author_id INTEGER NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES users (id);
+  FOREIGN KEY (author_id) REFERENCES users (id)
 );
 
 CREATE TABLE question_follows(
