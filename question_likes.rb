2268ad030082640f331fc2fb99333a35c249673e
David Veytsman <dveyts@yahoo.com>
2018-02-02 13:51:50 -0500

    find the users who like questions from the question_likes tables

--- question_likes.rb
+++ question_likes.rb
@@ -21,11 +21,27 @@ class QuestionLike
     SQL
     QuestionLike.new(question_like.first)
   end
-  
+
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
     @user_id = options['user_id']
+  end
+
+  def self.likers_for_question_id(question_id)
+    users = QuestionsDatabase.instance.execute(<<-SQL, question_id: question_id)
+      SELECT
+        *
+      FROM
+        users
+      JOIN
+        question_likes
+      ON
+        question_likes.user_id = users.id
+      WHERE
+        question_id = :question_id
+    SQL
+    users.map { |user| User.new(user) }
   end
 
 end
