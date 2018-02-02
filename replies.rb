91605244e5a5f5cee220e9761a44d71166375336
David Veytsman <dveyts@yahoo.com>
2018-02-01 19:57:43 -0500

    allows search for replies by question_id

--- replies.rb
+++ replies.rb
@@ -34,6 +34,18 @@ class Reply
     replies.map { |reply| Reply.new(reply) }
   end
 
+  def self.find_by_question_id(question_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, question_id: question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        question_id = :question_id
+    SQL
+    replies.map { |reply| Reply.new(reply)  }
+  end
+
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
