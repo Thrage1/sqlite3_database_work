ff6531a8e1129544d0c85c4391a13ccdd6bf9925
David Veytsman <dveyts@yahoo.com>
2018-02-06 13:52:35 -0500

    added method skeleton

---
+++ model_base.rb
@@ -0,0 +1,14 @@
+require 'SQLite3'
+require_relative 'questions_database'
+
+class ModelBase
+  def self.find_by_id
+  end
+
+  def self.all
+  end
+
+  def save
+  end
+  
+# you can use Object#instance_variables
