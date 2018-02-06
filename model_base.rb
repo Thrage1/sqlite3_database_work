c096697ea09f7d9d3077249ef057e961a99b1f95
David Veytsman <dveyts@yahoo.com>
2018-02-06 15:47:45 -0500

    added the modelbase with the find by id and also created the table variable

--- model_base.rb
+++ model_base.rb
@@ -2,7 +2,21 @@ require 'SQLite3'
 require_relative 'questions_database'
 
 class ModelBase
-  def self.find_by_id
+
+  def self.table
+    self.to_s.downcase + 's'
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id: id)
+      SELECT
+        *
+      FROM
+        #{table}
+      WHERE
+        id = :id
+    SQL
+    self.new(data.first)
   end
 
   def self.all
@@ -10,5 +24,5 @@ class ModelBase
 
   def save
   end
-  
+end
 # you can use Object#instance_variables
